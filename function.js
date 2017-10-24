6764cb4cc47780fe4b0d6284bfb1d39ea7a92647
Nicolas Pretre <nicolas.pretre.dev@gmail.com>
2017-10-24 11:50:47 +0200

    Envoi de l'exercice 8

---
+++ function.js
@@ -0,0 +1,9 @@
+function controle(form1){
+          var old = document.form1.age.value;
+          if (old >= 18) {
+                alert('Vous êtes majeur'); }
+
+          else {
+                alert('Vous êtes mineur');}
+
+      }
